f8edac79f20b324f96731b22ff7e75bf08c7be78
Alex H <hehe3301@gmail.com>
2015-04-24 10:19:25 -0400

    Begin shell methods

---
+++ src/model/TM.java
@@ -0,0 +1,9 @@
+package model;
+
+public class TM {
+	private char[] tape;
+	private int index;
+	private TMState currentState;
+	
+	
+}
---
+++ src/model/TMState.java
@@ -0,0 +1,22 @@
+package model;
+
+public class TMState {
+	private String name;
+	
+	
+	
+	public TMState(String name){
+		this.name=name;
+		
+	}
+	
+	
+
+	public boolean equals(TMState other){
+		
+		
+		
+		
+		return false;
+	}
+}
